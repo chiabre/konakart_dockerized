267e082ae25acbc69b80985945ccd2ed699f00cc
chiabre <luca.chiabrera@gmail.com>
2023-04-12 17:08:03 -0400

    konakart 9600

--- konakart_as_tomcat/build.sh
+++ konakart_as_tomcat/build.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-KONAKART_VERSION="9.4.0.1"
+KONAKART_VERSION="9.6.0.0"
+JMX_EXPORTER_VERSION="0.18.0"
+JVM_VERSION="17"
+IMG_VERSION="0.2"
 
-JMX_EXPORTER_VERSION="0.15.0"
-
-IMG_VERSION="0.1"
-
-for JAVA_VERSION in 8 11
-do
-    for JAVA_JVM in "" "-openj9"
-    do
-        docker build --build-arg JAVA_VERSION=${JAVA_VERSION} --build-arg JAVA_JVM=${JAVA_JVM} --build-arg KONAKART_VERSION=${KONAKART_VERSION} --build-arg JMX_EXPORTER_VERSION=${JMX_EXPORTER_VERSION} -t "chiabre/konakart_as_tomcat:${KONAKART_VERSION}-java${JAVA_VERSION}${JAVA_JVM}-${IMG_VERSION}" .
-        docker push "chiabre/konakart_as_tomcat:${KONAKART_VERSION}-java${JAVA_VERSION}${JAVA_JVM}-${IMG_VERSION}"
-    done
-done
+docker build --no-cache --progress=plain --build-arg KONAKART_VERSION=${KONAKART_VERSION} --build-arg JMX_EXPORTER_VERSION=${JMX_EXPORTER_VERSION} --build-arg JVM_VERSION=${JVM_VERSION} -t "chiabre/konakart_as_tomcat:${KONAKART_VERSION}-${JVM_VERSION}-${IMG_VERSION}" .
--- konakart_db_postgres/build.sh
+++ konakart_db_postgres/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POSTGRES_VERSION="13.2"
-KONAKART_VERSION="9.4.0.1"
+POSTGRES_VERSION="15.2"
+KONAKART_VERSION="9.6.0.0"
 
-docker build --build-arg KONAKART_VERSION=${KONAKART_VERSION} --build-arg MYSQL_VERSION=${POSTGRES_VERSION} -t "chiabre/konakart_db_postgres:${KONAKART_VERSION}_${POSTGRES_VERSION}" .
+docker build --no-cache --progress=plain --build-arg  KONAKART_VERSION=${KONAKART_VERSION} --build-arg POSTGRES_VERSION=${POSTGRES_VERSION} -t "chiabre/konakart_db_postgres:${KONAKART_VERSION}_${POSTGRES_VERSION}" .
